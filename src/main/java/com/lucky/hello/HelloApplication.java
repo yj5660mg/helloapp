7d1894ddef4df8e4823981024f1263be79b00564
Lucky Phuoc Pham <yj5660mg@go.minneapolis.edu>
2018-04-11 16:45:17 -0500

    firstwebapp

--- src/main/java/com/lucky/hello/HelloApplication.java
+++ src/main/java/com/lucky/hello/HelloApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @SpringBootApplication
 public class HelloApplication {
-
+//Homepage
 	@RequestMapping("/Hello")
 	@ResponseBody
 	String homepage() {
